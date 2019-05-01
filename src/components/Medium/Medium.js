1f624825e5f63a3b4ed5d1888de62fb10d2b85a2
Jeoffrey <jeoff.howe@gmail.com>
2019-05-01 13:55:46 -0600

    completed medium

--- src/components/Medium/Medium.js
+++ src/components/Medium/Medium.js
@@ -1,24 +1,29 @@
 import React, { Component } from 'react';
 import Card from './../shared/Card/Card';
+import {mediumArticles} from '../../ducks/mediumReducer'
 import Loading from './../shared/Loading/Loading';
+import {connect} from 'react-redux'
 
 class Medium extends Component {
-  constructor(props) {
-    super(props);
-    this.state = { articles: [], loading: true }
+  componentDidMount() {
+    this.props.mediumArticles();
   }
+  
   render() {
-    const articles = this.state.articles.map((article => <Card key={article.id} article={article} />))
+    const articles = this.props.articles.map((article => <Card key={article.id} article={article} />))
     return (
       <div className='news-container'>
         <img src="./mediumLogo.png" style={styles.logo} alt="" />
-        {this.state.loading ? <Loading /> : <div>{articles}</div>}
+        {this.props.loading ? <Loading /> : <div>{articles}</div>}
       </div>
     )
   }
 }
+function mapStateToProps(state) {
+  return state.medium
+}
 
-export default Medium;
+export default connect(mapStateToProps,{mediumArticles})(Medium);
 
 const styles = {
   logo: { width: '250px' }
--- src/ducks/mediumReducer.js
+++ src/ducks/mediumReducer.js
@@ -1,9 +1,25 @@
+import axios from 'axios'
 
 let initialState = {
     loading: false,
     articles: []
 }
 
+const MEDIUM_ARTICLES = 'MEDIUM_ARTICLES'
+
+export const mediumArticles = () => {
+    let articles = axios.get('/api/medium');
+    return { type: MEDIUM_ARTICLES,
+            payload: articles}
+}
 export default function(state = initialState, action) {
-    return state
+    switch(action.type) {
+        case `${MEDIUM_ARTICLES}_PENDING`:
+        return {...state, loading: true}
+        case `${MEDIUM_ARTICLES}_FULFILLED`:
+        return {...state,loading: false, articles: action.payload.data}
+        default:
+        return state
+    }
+    
 }
